28e8b6b097a9b8c59dbf6471a8fb86e5f0f7141c
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-11 03:53:25 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,12 @@
+def dictionary
+  dictionary = {"hello": "hi", "to": "2", "two": "2", "too": "2", "for": "4", "four": "4", "be": "b", "you": "u", "at": "@", "and": "&"}
+end
+  
+def word_substituter(string)
+  string.split(" ")
+end 
+
+
+
+
 # Write your code here.
